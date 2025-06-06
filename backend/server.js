ab3fe2c3e834f70ccdcdac900490a9097e80d5cf
tylersortijas <tylersortijas@gmail.com>
2025-06-06 02:11:18 -0700

    GET summary function complete

--- backend/server.js
+++ backend/server.js
@@ -96,9 +96,32 @@ app.delete("/api/transactions/:id", async (req, res) => {
   }
 });
 
+// GETS A SUMMARY OF TRANSACTIONS FOR A SPECIFIC USER
 app.get("/api/transactions/summary/:userId", async (req, res) => {
   try {
-  } catch (error) {}
+    const { userId } = req.params;
+
+    const balanceResult = await sql`
+    SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
+    `;
+
+    const incomeResult = await sql`
+    SELECT COALESCE(SUM(amount), 0) as income FROM transactions WHERE user_id = ${userId} AND amount > 0
+    `;
+
+    const expensesResult = await sql`
+    SELECT COALESCE(SUM(amount), 0) as income FROM transactions WHERE user_id = ${userId} AND amount < 0
+    `;
+
+    res.status(200).json({
+      balance: balanceResult[0].balance,
+      income: incomeResult[0].income,
+      expenses: expensesResult[0].income,
+    });
+  } catch (error) {
+    console.log(`Error getting the summary: ${error}`);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
 
 console.log("My port:", process.env.PORT);
