de89fe3ae155a4b00e7f976b398a7fca1e6b53c8
Francisco Roncalli <206579317+froncalli-softcrylic@users.noreply.github.com>
2025-06-09 14:52:59 -0700

    Styles/StyleSheets Implemented

---
+++ mobile/app/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Edit app/index.tsx to edit this screen.123</Text>
+    </View>
+  );
+}
