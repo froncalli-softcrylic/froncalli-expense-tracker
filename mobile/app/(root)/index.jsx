f4e1bd18b546fd0387e4eec58b4627a479844d1e
Francisco Roncalli <206579317+froncalli-softcrylic@users.noreply.github.com>
2025-06-10 22:03:58 -0700

    Home Page In-Progress

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,12 +1,18 @@
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SignOutButton } from "@/components/SignOutButton";
 import { useTransactions } from "../../hooks/useTransactions";
 import { useEffect } from "react";
+import PageLoader from "../../components/PageLoader";
+import { styles } from "../../assets/styles/home.styles";
+import { COLORS } from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import BalanceCard from "../../components/BalanceCard";
 
 export default function Page() {
   const { user } = useUser();
+  const router = useRouter();
   const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(
     user.id
   );
@@ -19,23 +25,51 @@ export default function Page() {
   console.log("summary:", summary);
   console.log("user:", user.id);
 
+  if (isLoading) return <PageLoader />;
+
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <Text>Income: {summary.income}</Text>
-        <Text>Balance: {summary.balance}</Text>
-        <Text>Expenses: {summary.expenses}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        {/* HEADER */}
+        <View style={styles.header}>
+          {/* LEFT */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              contentFit="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {/* tylersortijas@gmail.com */}
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+          {/* RIGHT */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity style={styles.addButton} onPress={() => router.push("/create")}>
+              <Ionicons name="add" size={20} color="#FFF" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+
+        <BalanceCard summary={summary} />
+
+        <View style={styles.transactionHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+      </View>
+      {/* FlatList is a performant way to render long lists in React Native. */}
+      {/* It renders items lazily - only those on the screen. */}
+      <FlatList
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionsListContent}
+        data={transactions}
+      />
     </View>
   );
 }
