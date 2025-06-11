e1e30d368841a0e186c34adec5e69919d5d7cbce
Francisco Roncalli <206579317+froncalli-softcrylic@users.noreply.github.com>
2025-06-10 19:12:55 -0700

    cron.js/server update implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,15 @@ import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
 
 // Middleware Section
 app.use(rateLimiter);
@@ -15,6 +20,10 @@ app.use(rateLimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Server is healthy", status: "OK" });
+});
 
 // Importing the url to the transactions route to use whenever the user accesses the /api/transactions endpoint
 app.use("/api/transactions", transactionsRoute);
--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
 
-const API_URL = "http://localhost:5001/api";
+const API_URL = "https://expense-tracker-api-7k3y.onrender.com/api";
 
 // react custom hook file
 export const useTransactions = (userId) => {
